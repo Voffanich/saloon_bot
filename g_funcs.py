af8be31e6bb49c7b44e48c0a7dc9d06f3637c874
Voffanich <vladimirmatushev@yandex.ru>
2022-11-25 17:38:38 +0300

    wip

--- g_funcs.py
+++ g_funcs.py
@@ -1,15 +1,19 @@
-def fact(a):
-    if a > 1:
-        return fact(a - 1) + fact(a - 2) + 3
-    else:
-        return a
+import pprint
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+class Google_calendar:
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
+    FILE_PATH ='user_data\saloon-bot-34fbbe2b1782.json'
     
-print(fact(1))
-print(fact(2))
-print(fact(3))
-print(fact(4))
-print(fact(5))
-print(fact(6))
-print(fact(7))
-print(fact(8))
-print(fact(9))
+    def __init__(self):
+        credentials = service_account.Credentials.from_service_account_file(filename = self.FILE_PATH, scopes = self.SCOPES)
+        self.service = build('calendar', 'v3', credentials = credentials)
+        
+    def get_calendar_list(self):
+        return self.sevice.calendar_list().list().execute()
+
+obj = Google_calendar()
+
+
+pprint.pprint(obj.get_calendar_list())
