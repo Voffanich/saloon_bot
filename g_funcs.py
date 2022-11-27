e2574da4f8f17eed589058189b2eed72a83d3dda
Voffanich <vladimirmatushev@yandex.ru>
2022-11-28 00:25:22 +0300

    wip

--- g_funcs.py
+++ g_funcs.py
@@ -11,9 +11,53 @@ class Google_calendar:
         self.service = build('calendar', 'v3', credentials = credentials)
         
     def get_calendar_list(self):
-        return self.sevice.calendar_list().list().execute()
+        return self.service.calendarList().list().execute()
+    
+    def add_calendar(self, calendar_id):
+        calendar_list_entry = {
+            'id': calendar_id            
+        }
+
+        return self.service.calendarList().insert(body=calendar_list_entry).execute()
+
+    def add_event(self, calendar_id, event):
+        
+        return self.service.events().insert(calendarId=calendar_id, body=event).execute()
+        # print('Event created: %s' % (event.get('htmlLink')))
+    
 
 obj = Google_calendar()
+calendar_id_1 = 'voffanich@gmail.com'
+calendar_id_2 = 'kazlova.alesia@gmail.com'
 
+# obj.add_calendar('kazlova.alesia@gmail.com')
 
-pprint.pprint(obj.get_calendar_list())
+# pprint.pprint(obj.get_calendar_list())
+
+event = {
+        'summary':'Аня Дубик',
+        # 'location': 'Минск',
+        'description': 'маникюр',
+        'start': {
+            'dateTime': '2022-11-28T15:00:00Z',
+            'timeZone': 'Europe/Minsk' 
+        },
+        'end': {
+            'dateTime': '2022-11-28T16:00:00Z',
+            'timeZone': 'Europe/Minsk' 
+        },      
+        'colorId':'7'                 
+        }  
+
+# event = obj.add_event(calendar_id=calendar_id, event=event)
+
+events = obj.service.events().list(calendarId=calendar_id_2, timeMin = '2022-11-01T00:00:00+03:00', timeMax = '2022-11-30T00:00:00+03:00').execute()
+# print(events)
+
+# for key, value in events.items():
+#         print(key, ' : ', value)
+    
+for item in events['items']:
+    print(item)
+    # print(item['summary'])
+    print('   ')
