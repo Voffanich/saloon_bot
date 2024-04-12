4559d64aac44ad5c5d60009e97324697bf5e4e35
Voffanich <vladimirmatushev@yandex.ru>
2024-04-12 17:56:35 +0300

    wip

--- g_funcs.py
+++ g_funcs.py
@@ -381,8 +381,8 @@ class Google_calendar:
         print(f'{manicure_stats["total_count"]=}, {manicure_stats["priced_count"]=}, {manicure_stats["sum"]=} ')
         print(f'{pedicure_stats["total_count"]=}, {pedicure_stats["priced_count"]=}, {pedicure_stats["sum"]=} ')
         
-        manicure_price = 59
-        pedicure_price = 52
+        manicure_price = 56
+        pedicure_price = 56
         
         if manicure_stats['total_count'] > 0:
             if manicure_stats['priced_count'] > 0:
